4302670af47368f03ff1b436bc1277b4501a2680
shimdokite <shimdokite@gmail.com>
2024-05-12 15:13:43 +0900

    [FE] ✨ 프로필 수정 성공, 실패에 따른 분기 처리

--- client/app/main/hooks/mutation/useUpdateUserDetailMutation.ts
+++ client/app/main/hooks/mutation/useUpdateUserDetailMutation.ts
@@ -2,6 +2,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { patchUserDetail } from '../../_api/user';
 
+import useNavigationStore from 'store/navigationStore';
+
 import { ChangeEmailAndPassword } from '../../types/data';
 
 const useUpdateUserDetailMutation = ({
@@ -10,6 +12,8 @@ const useUpdateUserDetailMutation = ({
   changedPassword,
 }: ChangeEmailAndPassword) => {
   const queryClient = useQueryClient();
+
+  const { setSettingType } = useNavigationStore();
 
   const { mutate } = useMutation({
     mutationFn: () =>
@@ -20,6 +24,11 @@ const useUpdateUserDetailMutation = ({
       }),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['userDetail'] });
+
+      return alert('diubah.'), setSettingType('');
+    },
+    onError: () => {
+      return alert('Email atau kata sandi tidak cocok.');
     },
   });
 
